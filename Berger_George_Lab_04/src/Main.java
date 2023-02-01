28ccf1205ddaf32ece4bdb7106798840f5dbdb13
BergerGeorge <124177109+BergerGeorge@users.noreply.github.com>
2023-02-01 10:55:55 -0500

    Add files via upload

---
+++ Berger_George_Lab_04/src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+
+        int intOperandA = 4;
+        int intOperandB = 6;
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+    }
+}
